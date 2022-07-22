d78cfe1133e53f870d7176e8ae3e93478cf9c8b3
MervanKoncuk <mervankoncuk@gmail.com>
2022-07-22 14:38:05 +0300

    spaces

--- ogrenciler/forms.py
+++ ogrenciler/forms.py
@@ -11,6 +11,8 @@ class OgrenciForm(ModelForm):
     def __init__(self, *arg, **kwargs):
         super(OgrenciForm, self).__init__(*arg, **kwargs)
 
+
+
         self.fields['isim_soyisim'].widget.attrs.update({'class': 'form-control', 'placeholder': 'İsim ve Soyismi giriniz.'})
         self.fields['hakkimda'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Kendinizi tanıtınız.'})
         self.fields['resim'].widget.attrs.update({'class': 'form-control'})
